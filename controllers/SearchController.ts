2a3d40830389ff1ad43fe59be43122c513be3f62
Igor <devNoon@yandex.ru>
2022-05-13 23:18:40 +0300

    Tag cloud

--- controllers/SearchController.ts
+++ controllers/SearchController.ts
@@ -72,21 +72,26 @@ class SearchController {
   }
 
   async countTags(req: express.Request, res: express.Response) {
-    const tags = new Map();
+    const MAX_TAG_NUMBER_IN_CLOUD = 15;
+    const tagsMap = new Map();
     try {
       const items = await Item.findAll();
       for (let item of items) {
         for (let tag of item.tags) {
-          if (!tags.has(tag)) {
-            tags.set(tag, 0);
+          if (!tagsMap.has(tag)) {
+            tagsMap.set(tag, 0);
           }
 
-          const number = tags.get(tag);
-          tags.set(tag, number + 1);
+          const number = tagsMap.get(tag);
+          tagsMap.set(tag, number + 1);
         }
       }
-
-      res.send({ ...Object.fromEntries(tags) });
+      const tags: any[] = [];
+      tagsMap.forEach((value, key) => {
+        tags.push({ value: key, count: value });
+      });
+      tags.sort((a, b) => b.count - a.count);
+      res.send({ tags: tags.splice(0, MAX_TAG_NUMBER_IN_CLOUD) });
     } catch (error) {
       console.log(error);
       res
